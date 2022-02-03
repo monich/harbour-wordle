28f35a5845576911f5ea3b640bc265f31f2bc645
1Zgp <36508793+1Zgp@users.noreply.github.com>
2022-02-03 04:16:47 +0200

    [wordle] Hungarian dictionary and translation

---
+++ translations/harbour-wordle-hu.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hu">
+<context>
+    <name></name>
+    <message id="wordle-menu-new_game">
+        <source>New game</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Új játék</translation>
+    </message>
+    <message id="wordle-settings-header">
+        <source>Settings</source>
+        <extracomment>Page header</extracomment>
+        <translation>Beállítások</translation>
+    </message>
+    <message id="wordle-settings-language-label">
+        <source>Dictionary</source>
+        <extracomment>Combo box label</extracomment>
+        <translation>Szótár</translation>
+    </message>
+    <message id="wordle-settings-keep_display_on">
+        <source>Keep display on while playing</source>
+        <extracomment>Text switch label</extracomment>
+        <translation>Kijelző ébrentartása a játék ideje alatt</translation>
+    </message>
+</context>
+</TS>
