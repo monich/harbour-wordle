19fbfca117c4125fa20947b1763a2ba67e7d25a0
Åke Engelbrektson <eson57@users.noreply.github.com>
2022-02-03 16:41:33 +0100

    Create harbour-wordle-se.ts
    Hi,
    Here's a Swedish translation and required word list files.

---
+++ translations/harbour-wordle-se.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name></name>
+    <message id="wordle-menu-new_game">
+        <source>New game</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Nytt spel</translation>
+    </message>
+    <message id="wordle-settings-header">
+        <source>Settings</source>
+        <extracomment>Page header</extracomment>
+        <translation>Inställningar</translation>
+    </message>
+    <message id="wordle-settings-language-label">
+        <source>Dictionary</source>
+        <extracomment>Combo box label</extracomment>
+        <translation>Ordbok</translation>
+    </message>
+    <message id="wordle-settings-keep_display_on">
+        <source>Keep display on while playing</source>
+        <extracomment>Text switch label</extracomment>
+        <translation>Håll skärmen tänd under spel</translation>
+    </message>
+</context>
+</TS>
