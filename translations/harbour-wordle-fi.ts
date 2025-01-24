05e42ad3699c7e0ed4a8e640ba223b780a47edfb
Elmeri Länsiharju <139926936+tuplasuhveli@users.noreply.github.com>
2025-01-24 21:00:29 +0000

    Update harbour-wordle-fi.ts
    Completed the translation and checked the previously translated strings.

--- translations/harbour-wordle-fi.ts
+++ translations/harbour-wordle-fi.ts
@@ -16,22 +16,22 @@
     <message id="wordle-menu-new_game">
         <source>New game</source>
         <extracomment>Pulley menu item</extracomment>
-        <translation type="unfinished">Uusi peli</translation>
+        <translation>Uusi peli</translation>
     </message>
     <message id="wordle-settings-header">
         <source>Settings</source>
         <extracomment>Page header</extracomment>
-        <translation type="unfinished">Asetukset</translation>
+        <translation>Asetukset</translation>
     </message>
     <message id="wordle-settings-language-label">
         <source>Dictionary</source>
         <extracomment>Combo box label</extracomment>
-        <translation type="unfinished">Sanakirja</translation>
+        <translation>Sanakirja</translation>
     </message>
     <message id="wordle-settings-language-description" numerus="yes">
         <source>%1 words</source>
         <extracomment>Language descriptions (word count)</extracomment>
-        <translation type="unfinished">
+        <translation>
             <numerusform>%1 sana</numerusform>
             <numerusform>%1 sanaa</numerusform>
         </translation>
@@ -39,7 +39,7 @@
     <message id="wordle-settings-language-description2" numerus="yes">
         <source>%1 + %2 words</source>
         <extracomment>Language descriptions (word counts)</extracomment>
-        <translation type="unfinished">
+        <translation>
             <numerusform>%1 + %2 sana</numerusform>
             <numerusform>%1 + %2 sanaa</numerusform>
         </translation>
@@ -67,17 +67,17 @@
     <message id="wordle-settings-show_play_time">
         <source>Show timer</source>
         <extracomment>Text switch label</extracomment>
-        <translation type="unfinished">Näytä ajastin</translation>
+        <translation>Näytä ajastin</translation>
     </message>
     <message id="wordle-settings-keep_display_on">
         <source>Keep display on while playing</source>
         <extracomment>Text switch label</extracomment>
-        <translation type="unfinished">Pidä näyttö päällä pelaamisen aikana</translation>
+        <translation>Pidä näyttö päällä pelaamisen aikana</translation>
     </message>
     <message id="wordle-settings-keep_display_on-description">
         <source>To avoid completely discharging the battery, display blanking would still be allowed if the battery level drops below %1% and the phone is not on charger.</source>
         <extracomment>Text switch label description</extracomment>
-        <translation type="unfinished">Jotta akku ei tyhjene kokonaan, näytön sammuttaminen on silti sallittu, jos akun varaus putoaa alle %1% ja puhelin ei lataudu.</translation>
+        <translation>Jotta akku ei tyhjene kokonaan, näytön sammuttaminen on sallittu, jos akun varaus putoaa alle %1 prosentin ja puhelin ei ole latauksessa.</translation>
     </message>
     <message id="wordle-settings-version">
         <source>Version %1</source>
@@ -87,57 +87,57 @@
     <message id="wordle-statistics-header">
         <source>Statistics</source>
         <extracomment>Page header</extracomment>
-        <translation type="unfinished">Tilastot</translation>
+        <translation>Tilastot</translation>
     </message>
     <message id="wordle-statistics-total_time">
         <source>Total time</source>
         <extracomment>Statistics item label</extracomment>
-        <translation type="unfinished">Kokonaisaika</translation>
+        <translation>Kokonaisaika</translation>
     </message>
     <message id="wordle-statistics-best_time">
         <source>Best time</source>
         <extracomment>Statistics item label</extracomment>
-        <translation type="unfinished">Paras aika</translation>
+        <translation>Paras aika</translation>
     </message>
     <message id="wordle-statistics-games_played">
         <source>Games played</source>
         <extracomment>Statistics item label</extracomment>
-        <translation type="unfinished">Pelejä pelannut</translation>
+        <translation>Pelejä pelattu</translation>
     </message>
     <message id="wordle-statistics-win_percentage">
         <source>Win %</source>
         <extracomment>Statistics item label</extracomment>
-        <translation type="unfinished">Voitto %</translation>
+        <translation>Voittoprosentti</translation>
     </message>
     <message id="wordle-statistics-current_streak">
         <source>Current streak</source>
         <extracomment>Statistics item label</extracomment>
-        <translation type="unfinished">Nykyinen voittoputki</translation>
+        <translation>Nykyinen voittoputki</translation>
     </message>
     <message id="wordle-statistics-longest_streak">
         <source>Longest streak</source>
         <extracomment>Statistics item label</extracomment>
-        <translation type="unfinished">Pisin voittoputki</translation>
+        <translation>Pisin voittoputki</translation>
     </message>
     <message id="wordle-history-guess_distribution">
         <source>Guess distribution</source>
         <extracomment>Guess distribution historgram title</extracomment>
-        <translation type="unfinished">Arvausten jakautuminen</translation>
+        <translation>Arvausten jakautuminen</translation>
     </message>
     <message id="wordle-history-header">
         <source>History</source>
         <extracomment>Page header</extracomment>
-        <translation type="unfinished">Historia</translation>
+        <translation>Historia</translation>
     </message>
     <message id="wordle-history-menu-clear">
         <source>Clear history</source>
         <extracomment>Pulley menu item</extracomment>
-        <translation type="unfinished">Tyhjennä historia</translation>
+        <translation>Tyhjennä historia</translation>
     </message>
     <message id="wordle-history-clear-remorse">
         <source>All history and statistics will be deleted</source>
         <extracomment>Remorse popup description</extracomment>
-        <translation type="unfinished">Kaikki historia ja tilastot poistetaan</translation>
+        <translation>Koko historia ja tilastot poistetaan</translation>
     </message>
 </context>
 </TS>
