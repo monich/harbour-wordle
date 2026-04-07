8933c0dd128111a96c71742b97ac20aa5504d410
Frank Paul Silye <frankps@gmail.com>
2026-04-07 14:21:41 +0200

    [translations] Norwegian Bokmål translation

---
+++ translations/harbour-wordle-no_NB.ts
@@ -0,0 +1,163 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nb_NO">
+<context>
+    <name></name>
+    <message id="wordle-generic-yes">
+        <source>Yes</source>
+        <extracomment>Generic answer</extracomment>
+        <translation>Ja</translation>
+    </message>
+    <message id="wordle-generic-no">
+        <source>No</source>
+        <extracomment>Generic answer</extracomment>
+        <translation>Nei</translation>
+    </message>
+    <message id="wordle-menu-new_game">
+        <source>New game</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Nytt spill</translation>
+    </message>
+    <message id="wordle-menu-whats_this">
+        <source>What&apos;s this?</source>
+        <extracomment>Pulley and context menu item</extracomment>
+        <translation>Hva er dette?</translation>
+    </message>
+    <message id="wordle-settings-header">
+        <source>Settings</source>
+        <extracomment>Page header</extracomment>
+        <translation>Innstillinger</translation>
+    </message>
+    <message id="wordle-settings-language-label">
+        <source>Dictionary</source>
+        <extracomment>Combo box label</extracomment>
+        <translation>Ordbok</translation>
+    </message>
+    <message id="wordle-settings-language-description" numerus="yes">
+        <source>%1 words</source>
+        <extracomment>Language descriptions (word count)</extracomment>
+        <translation>
+            <numerusform>%1 ord</numerusform>
+            <numerusform>%1 ord</numerusform>
+        </translation>
+    </message>
+    <message id="wordle-settings-language-description2" numerus="yes">
+        <source>%1 + %2 words</source>
+        <extracomment>Language descriptions (word counts)</extracomment>
+        <translation>
+            <numerusform>%1 + %2 ord</numerusform>
+            <numerusform>%1 + %2 ord</numerusform>
+        </translation>
+    </message>
+    <message id="wordle-settings-orientation-label">
+        <source>Orientation</source>
+        <extracomment>Combo box label</extracomment>
+        <translation>Retning</translation>
+    </message>
+    <message id="wordle-settings-orientation-dynamic">
+        <source>Dynamic</source>
+        <extracomment>Combo box value for dynamic orientation</extracomment>
+        <translation>Dynamisk</translation>
+    </message>
+    <message id="wordle-settings-orientation-portrait">
+        <source>Portrait</source>
+        <extracomment>Combo box value for portrait orientation</extracomment>
+        <translation>Stående (portrett)</translation>
+    </message>
+    <message id="wordle-settings-orientation-landscape">
+        <source>Landscape</source>
+        <extracomment>Combo box value for landscape orientation</extracomment>
+        <translation>Liggende (landskap)</translation>
+    </message>
+    <message id="wordle-settings-show_play_time">
+        <source>Show timer</source>
+        <extracomment>Text switch label</extracomment>
+        <translation>Vis tidtaker</translation>
+    </message>
+    <message id="wordle-settings-keep_display_on">
+        <source>Keep display on while playing</source>
+        <extracomment>Text switch label</extracomment>
+        <translation>Hold skjermen på under spilling</translation>
+    </message>
+    <message id="wordle-settings-keep_display_on-description">
+        <source>To avoid completely discharging the battery, display blanking would still be allowed if the battery level drops below %1% and the phone is not on charger.</source>
+        <extracomment>Text switch label description</extracomment>
+        <translation>For å unngå at batteriet tømmes helt, vil skjermdvalen fortsatt aktiveres hvis batterinivået faller under %1 % og telefonen ikke er koblet til lader.</translation>
+    </message>
+    <message id="wordle-settings-show_whats_this">
+        <source>Enable «What&apos;s this» menu</source>
+        <extracomment>Text switch label</extracomment>
+        <translation>Aktiver «Hva er dette»-menyen</translation>
+    </message>
+    <message id="wordle-settings-show_whats_this-description">
+        <source>If the word which you were guessing isn&apos;t familiar to you, this function allows you to quickly look up the meaning of the word in the selected search engine.</source>
+        <extracomment>Text switch label description</extracomment>
+        <translation>Hvis ordet du gjettet er ukjent for deg, lar denne funksjonen deg raskt slå opp betydningen av ordet i den valgte søkemotoren.</translation>
+    </message>
+    <message id="wordle-settings-search_engine-label">
+        <source>Search engine</source>
+        <extracomment>Combo box label</extracomment>
+        <translation>Søkemotor</translation>
+    </message>
+    <message id="wordle-settings-version">
+        <source>Version %1</source>
+        <extracomment>Small description label (app version)</extracomment>
+        <translation>Versjon %1</translation>
+    </message>
+    <message id="wordle-statistics-header">
+        <source>Statistics</source>
+        <extracomment>Page header</extracomment>
+        <translation>Statistikk</translation>
+    </message>
+    <message id="wordle-statistics-total_time">
+        <source>Total time</source>
+        <extracomment>Statistics item label</extracomment>
+        <translation>Total tid</translation>
+    </message>
+    <message id="wordle-statistics-best_time">
+        <source>Best time</source>
+        <extracomment>Statistics item label</extracomment>
+        <translation>Beste tid</translation>
+    </message>
+    <message id="wordle-statistics-games_played">
+        <source>Games played</source>
+        <extracomment>Statistics item label</extracomment>
+        <translation>Spilte spill</translation>
+    </message>
+    <message id="wordle-statistics-win_percentage">
+        <source>Win %</source>
+        <extracomment>Statistics item label</extracomment>
+        <translation>Seier %</translation>
+    </message>
+    <message id="wordle-statistics-current_streak">
+        <source>Current streak</source>
+        <extracomment>Statistics item label</extracomment>
+        <translation>Nåværende rekke</translation>
+    </message>
+    <message id="wordle-statistics-longest_streak">
+        <source>Longest streak</source>
+        <extracomment>Statistics item label</extracomment>
+        <translation>Lengste rekke</translation>
+    </message>
+    <message id="wordle-history-guess_distribution">
+        <source>Guess distribution</source>
+        <extracomment>Guess distribution historgram title</extracomment>
+        <translation>Fordeling av gjettinger</translation>
+    </message>
+    <message id="wordle-history-header">
+        <source>History</source>
+        <extracomment>Page header</extracomment>
+        <translation>Historikk</translation>
+    </message>
+    <message id="wordle-history-menu-clear">
+        <source>Clear history</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Tøm historikk</translation>
+    </message>
+    <message id="wordle-history-clear-remorse">
+        <source>All history and statistics will be deleted</source>
+        <extracomment>Remorse popup description</extracomment>
+        <translation>All historikk og statistikk vil bli slettet</translation>
+    </message>
+</context>
+</TS>
